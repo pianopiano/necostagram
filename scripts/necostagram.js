e95d5d0d52b462cb51889dad2f4a4598c46e5752
pianopiano <mail.pianopiano@gmail.com>
2013-04-19 01:23:52 +0900

    修正

--- scripts/necostagram.js
+++ scripts/necostagram.js
@@ -5,24 +5,26 @@
 (function(){
 	$(function(){
 		var sobj
+		,	$header=$('#header')
 		,	$necoLoader=$('#necoLoader')
 		,	$necoContainer=$('#necoContainer')
 		,	$pageTop=$('#pageTop')
 		,	$thumbneco=$('.thumbneco')
 		,	$win=$(window)
+		,	$sns = $('#sns')
 		,	max=40
-		,	_isAnimated=true	
+		,	_isAnimated=true
 		,	loaded=false
 		,	isJump=false
+		,	ios = false
 		,	IDs=[]
 		,	necoTag = 'ねこ'
 		,	pTopImgs=['images/j1.gif','images/j2.gif','images/j3.gif']
-		,	$sns = $('#sns')
-		,	ios = false
 		,	snd=1
 		,	sndfmt='.mp3'
-		,	sndPath='sound/n';
-		
+		,	sndPath='sound/n'
+		,	cookieValue=''
+		,	num=0;
 		
 		var audio = new Audio();
         if      (audio.canPlayType("audio/ogg") == 'maybe') { sndfmt = '.ogg'; }
@@ -31,7 +33,7 @@
 		audio=null;
 		
 		function sndPlay() {
-			snd += 1;
+			snd++;
 			if (snd==7)snd=1;
 			(new Audio(sndPath+snd+sndfmt)).play();
 		}
@@ -42,10 +44,10 @@
 		if ((agent.indexOf('iPhone') > 0 && agent.indexOf('iPad') == -1) || agent.indexOf('iPad') > 0 || agent.indexOf('Android') > 0) {
 			isIos();
 		} else {
-			is_pc();
+			isPc();
 		}
 		
-		function is_ios() {
+		function isIos() {
 			ios = true;
 			max=19;
 			if (navigator.userAgent.indexOf('iPad') > 0){
@@ -59,43 +61,40 @@
 				'transition-duration':'0s'
 			});
 			_isAnimated=false;
-		};
+		}
 		
-		function is_pc() {
+		function isPc() {
 			$thumbneco.css({
 				'-webkit-mask-image':'url(../images/circle.png)',
 				'-webkit-mask-size':'280px',
 				'-webkit-mask-repeat':'no-repeat',
 				'-webkit-mask-position':'center'
-			});
-		};
+			})
+		}
 		
 		$necoLoader.css({'left':$win.width()/2-30+'px','top':($win.height()/2)-100+'px'}).fadeIn(500);
 		$win.resize(resizeHandler);
 		necoLoad();
-
+		
 		function necoLoad(){
 			setWidth ();
 			$.ajax({
 				url: "https://api.instagram.com/v1/tags/"+necoTag+"/media/recent?client_id=f39149070d2d4c5fb73cdddcaf00e0dd",
 				data:{count:max.toString()},
 				dataType: "jsonp",
-				error: function(jqXHR,textStatus,errorThrown){
-					alert('にゃーにゃーにゃー（'+textStatus+'）')
-					//$necoContainer.text(textStatus);
-				},
+				error: function(jqXHR,textStatus,errorThrown){$necoContainer.text(textStatus);},
 				success: function(data,textStatus,jqXHR){
 					necoBuild(data);
 				}
 			});
-		};
-		
+		}
 		
 		function necoBuild(data){	
 			var _neco=data
 			,	i
 			,	len=0;
 			for (i=0; i < max; i++){
+				
 				var user
 				,	link
 				,	url
@@ -118,19 +117,19 @@
 				if (link!='#'||url!='#'){
 					var content=	'<div class="box image shadow" id="'+_data.id+'">'+
 										'<a href="'+link+'" target="_blank">'+
-											'<img class="thumbneco" src="'+url+'" width="200" />'+
-										'</a><br />'+
+										'<img class="thumbneco" src="'+url+'" width="200" /></a><br />'+
 										'<div class="description">'+
 											'<a href="http://instagram.com/'+user.username+'" target="_blank">'+
 												'<img class="thumbnail" src="'+user.profile_picture+'" width="30" />'+
 												'<p class="username"><strong>'+user.username+'</strong></p>'+
 											'</a>'+
 											'<p class="caption">'+capsText+'</p>'+
-											'<p class="like"></p>'+
+											//'<p class="like"></p>'+
 										'</div>'+
 									'</div>';
 					if (max!=1){
 						len += 1;
+						num=len;
 						$necoContainer.append(content);
 						IDs.push(user.id);
 						if (len==(max-1)){};
@@ -150,7 +149,7 @@
 			setTimeout(function(){
 				max=1;
 				necoLoad();
-			},5000);
+			},7000);
 		};
 		
 		function setWidth(){
@@ -162,8 +161,10 @@
 			if ($ww<480){$necoContainer.width(200);} else{};
 			
 			if ($('#profPict')){
-				$('#profPict').css({'left':($win.width()/2+$necoContainer.width()/2)-50+'px'});
+				$('#profPict').css({'right':0+'px'});
+				$('#userContainer').css({'left':($win.width()/2+$necoContainer.width()/2)-15+'px'})
 			}
+			
 		}
 		
 		function resizeHandler(){
@@ -201,91 +202,27 @@
 	    });
 	    
 	    
-	    // login
-	    function setupLike(accessToken){
-			$('.like').show();
-			$('.like').live('click',function(){
-				var like=false
-				,	id=$(this).parent('.description').parent('.box').attr('id')
-				,	src = $(this).css('background-image').split('like')
-				,	accessType = '';
-				
-				if (src[1]=='.png)'){
-					$(this).css({'background-image':src[0]+'like_x.png)'});
-					like = true;
-				} else if (src[1]=='_x.png)') {
-					$(this).css({'background-image':src[0]+'like.png)'});
-					like = false;
-				}
-				
-				if (like == true){
-					accessType = 'POST';
-				} else if (like == false) {
-					accessType = 'DELETE';
-				};
-				
-				$.ajax({
-					type: 'POST',
-					url: "likes.php",
-					data: { access_token: accessToken, media_id: id ,type: accessType},
-					success: function(data, dataType) {
-						console.log('success.data = '+data);
-					},
-					error: function(XMLHttpRequest, textStatus, errorThrown){
-						alert('error.XMLHttpRequest = ' + errorThrown);
-					}
-				})	
-				
-			})
-	    }
-	    
 		window.onload=function(){
 			$('.like').hide();
 			sndPlay();
-			$('#header').animate({'top':'0'},1000);
+			$header.animate({'top':'0'},1000);
 			$necoLoader.fadeOut(1000,function(){
 				$necoContainer.append($sns)
 				$necoLoader.remove();
 				$necoContainer.masonry({itemSelector:'.box',isAnimated:_isAnimated});
 				loaded = true;
-				
-				$sns.fadeIn();	
+				var comNum=0;
+				$sns.fadeIn();
 				for (var i=0; i<max;i++){
-					$necoContainer.find('.image').eq(i).delay(i*20).animate({'opacity':'1'},0,'swing');
+					$necoContainer.find('.image').eq(i).delay(i*20).animate({'opacity':'1'},0,'swing',function(){
+						comNum++;
+						if (comNum==num-1) {
+							addMouseEvents();
+						}
+						
+					});
 				}
 				
-				if (ios==false){
-					$thumbneco.live('mouseover',function(){
-						$(this).stop().transition({"-webkit-mask-size":"200px"}, 50, 'ease-out')
-							.css({ transform: "rotate(5deg)" }).closest('.image').stop().transition({
-								'background-color':'#f2f0f0',
-								'-webkit-border-top-left-radius':'110px','-moz-border-radius-topleft':'110px',
-								'-webkit-border-top-right-radius':'110px','-moz-border-radius-topright':'110px'
-						}, 50, 'ease-out');
-					}).live('mouseout',function(){
-						$(this).stop().transition({"-webkit-mask-size":"280px"}, 30, 'ease-out')
-							.css({ transform: "rotate(0deg)" }).closest('.image').stop().transition({
-								'background-color':'#ffffff',
-								'-webkit-border-top-left-radius':'10px','-moz-border-radius-topleft':'10px',
-								'-webkit-border-top-right-radius':'10px','-moz-border-radius-topright':'10px'
-						}, 30, 'ease-out');
-					});
-					
-					$('#icon').hover(function(){
-						$(this).fadeTo(0,0.8);
-						$('#me').fadeIn(200);
-					},function(){
-						$(this).fadeTo(0,1);
-						$('#me').fadeOut(200);
-					}).click(function(){
-						$('#me').fadeOut(0);
-					});
-					$('.description').hover(function(){
-						$(this).find('.thumbnail').css({'border':'1px solid #8c7e7e'}).end().find('.username').css({'color':'#8c7e7e'});
-					},function(){
-						$(this).find('.thumbnail').css({'border':'1px solid #ccc'}).end().find('.username').css({'color':'#444444'});
-					});
-				}
 			
 				$thumbneco.live('click', function(){
 					sndPlay();
@@ -294,23 +231,167 @@
 				
 			});
 			
-			if (accessToken==0){
-				//setupLogin()
-			} else {
-				setupLike(accessToken);
-			}
-			
-			// user 
-			if ($profile_picture) {
-				$header.append('<img id="profPict" src="'+$profile_picture+'" width="30" height="30" />');
-				
-				$('#profPict').css({'position':'absolute','left':($win.width()/2+$necoContainer.width()/2)-50+'px','top':'9px','border':'2px solid #ccc'});
-			}
+			getCookie();
+			setCookie();
 			
 		}
+		function addMouseEvents() {
+			resizeHandler();
+			if (ios==false){
+				$thumbneco.live('mouseover',function(){
+					$(this).stop().transition({"-webkit-mask-size":"200px"}, 50, 'ease-out')
+						.css({ transform: "rotate(5deg)" }).closest('.image').stop().transition({
+							'background-color':'#f2f0f0',
+							'-webkit-border-top-left-radius':'110px','-moz-border-radius-topleft':'110px',
+							'-webkit-border-top-right-radius':'110px','-moz-border-radius-topright':'110px'
+					}, 50, 'ease-out');
+				}).live('mouseout',function(){
+					$(this).stop().transition({"-webkit-mask-size":"280px"}, 30, 'ease-out')
+						.css({ transform: "rotate(0deg)" }).closest('.image').stop().transition({
+							'background-color':'#ffffff',
+							'-webkit-border-top-left-radius':'10px','-moz-border-radius-topleft':'10px',
+							'-webkit-border-top-right-radius':'10px','-moz-border-radius-topright':'10px'
+					}, 30, 'ease-out');
+				});
+				
+				$('#icon').hover(function(){
+					$(this).fadeTo(0,0.8);
+					$('#me').fadeIn(200);
+				},function(){
+					$(this).fadeTo(0,1);
+					$('#me').fadeOut(200);
+				}).click(function(){
+					$('#me').fadeOut(0);
+				});
+				$('.description').hover(function(){
+					$(this).find('.thumbnail').css({'border':'1px solid #8c7e7e'}).end().find('.username').css({'color':'#8c7e7e'});
+				},function(){
+					$(this).find('.thumbnail').css({'border':'1px solid #ccc'}).end().find('.username').css({'color':'#444444'});
+				});
+			}
+		}
+	    // login
+	    function setupLike($accessToken){
+			$('.like').show().live('click',function(){
+				var like = false;
+				var id = $(this).parent('.description').parent('.box').attr('id');
+				var src = $(this).css('background-image').split('like');
+				if (src[1]=='.png)'){
+					$(this).css({'background-image':src[0]+'like_x.png)'});
+					like = true;
+				} else if (src[1]=='_x.png)') {
+					$(this).css({'background-image':src[0]+'like.png)'});
+					like = false;
+				}
+				var accessType = '';
+				if (like == true){
+					accessType = 'POST';
+				} else if (like == false) {
+					accessType = 'DELETE';
+				};
+				
+				
+				$.ajax({
+					type: 'POST',
+					url: "likes.php",
+					data: { access_token: $accessToken, media_id: id ,type: accessType},
+					success: function(data, dataType) {
+						//console.log('OK = '+data);
+					},
+					error: function(XMLHttpRequest, textStatus, errorThrown){
+						//alert('error = ' + errorThrown);
+					}
+				})	
+				
+			})
+	    }
+	    
+		function getCookie() {
+			var data = $accessToken+'/'+$username;
+			if (document.cookie) {
+				var cookies = document.cookie.split("; ");
+				for (var i = 0; i < cookies.length; i++) {
+					var str = cookies[i].split("=");
+					if (str[0] == cookieName) {
+						var cookie_value = unescape(str[1]);
+						cookieValue = unescape(str[1])
+						getUser(cookie_value);
+						if (!isNaN($accessToken)) $accessToken = ++cookie_value;
+						break;
+					}
+				}
+			}
+		}
+		
+		function getUser(cv) {
+			var cvs = cv.split('/');
+			$.ajax({
+				type: 'POST',
+				url: "https://api.instagram.com/v1/users/search?q="+cvs[1]+'&access_token='+cvs[0],
+				dataType: "jsonp",
+				success: function(data, dataType) {
+					$username = data.data[0].username;
+					$profile_picture = data.data[0].profile_picture;
+					$id = data.data[0].id;
+					buildUser();
+				},
+				error: function(XMLHttpRequest, textStatus, errorThrown){
+					alert('ログイン失敗');
+					
+				}
+			})	
+		}
+		
+		function setCookie(){
+			if (!cookieValue) {
+				setupLogin()
+			}
+		}
+		
+		function delCookie(){
+			var date1 = new Date();
+			date1.setTime(0);
+			document.cookie = cookieName+"=;expires="+date1.toGMTString();
+			$('#profPict,#userName').empty().remove();
+			location.reload();
+		}
+		
+		function buildUser() {
+			if ($profile_picture) {
+				setupLike($accessToken);
+				$header.append(
+					'<div id="userContainer">'+
+						'<div id="userName"><p>'+$username+'</p></div>'+
+						'<img id="profPict" src="'+$profile_picture+'" width="30" height="30" />'+
+					'</div>');
+				$('#profPict,#userName').hide();
+				setTimeout(function(){
+					$('#profPict').css({
+						'position':'absolute',
+						'right':0+'px',
+						'top':'0px',
+						'border':'2px solid #ccc'
+					}).click(delCookie).fadeIn(300);
+					$('#userContainer').css({
+						'position':'absolute',
+						'left':($win.width()/2+$necoContainer.width()/2)-15+'px',
+						'top':'9px'
+					})
+					$('#userName').css({
+						'position':'absolute',
+						'right':45+'px',
+						'top':'8px',
+						'text-align':'right !important',
+						'font-size':'14px !important'
+					}).fadeIn(300);
+					
+				}, 1000);
+			}
+		}
+		
 		function setupLogin() {
-			$('#login').fadeIn().animate({'right':'20px'}, 500,'swing').click(function(){
-				var client_id = 'f39149070d2d4c5fb73cdddcaf00e0dd';
+			$('#login')/*.fadeIn()*/.animate({'right':'20px'}, 500,'swing').click(function(){
+				var client_id = '28977a1a96d94967a2cb14709492c46d';
 				var redirect_uri = 'http://necostagram.com/';
 				location.href = 'https://api.instagram.com/oauth/authorize/?client_id='+client_id+'&redirect_uri='+redirect_uri+'&response_type=code&scope=likes';
 			});
